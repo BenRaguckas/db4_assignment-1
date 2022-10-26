0a568ec01f781e013c32721af2cceb2637af459c
Ben Raguckas <benraguckas@gmail.com>
2022-10-26 11:20:03 +0100

    Added sql dataset. Minor code rafactoring.

---
+++ resources/create.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS android_apps;
+CREATE TABLE android_apps(
+   id             INTEGER        NOT NULL PRIMARY KEY,
+   app            VARCHAR(256)   NOT NULL,
+   category       VARCHAR(64)    NOT NULL,
+   rating         DECIMAL(7,1),
+   reviews        INTEGER        NOT NULL,
+   size           DECIMAL(7,1),
+   installs       VARCHAR(32)    NOT NULL,
+   type           VARCHAR(32)    NOT NULL,
+   price          VARCHAR(32)    NOT NULL,
+   content_rating VARCHAR(32)    NOT NULL,
+   genres         VARCHAR(256)   NOT NULL,
+   last_updated   DATE           NOT NULL,
+   current_ver    VARCHAR(64),
+   android_ver    VARCHAR(64)
+);
